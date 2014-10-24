1231d8882507303aaef81dcfa1cdd4e04f21de33
Smitha Milli <smitha.milli@gmail.com>
2014-10-24 13:21:51 -0700

    Solve problems

--- crt.js
+++ crt.js
@@ -2,6 +2,10 @@ var Crt = function(numEquations) {
   this.equations = {};
   this.primes = [];
   this._generateProblem(numEquations);
+  this.primesProd = 1;
+  for (var i = 0; i < this.primes.length; i++) {
+    this.primesProd *= this.primes[i];
+  }
 }
 
 
@@ -26,13 +30,90 @@ Crt.prototype._generateProblem = function(numEquations) {
 }
 
 
+/**
+ * Finds the multiplicitive inverse of a mod m.
+ * @returns {Number} - the multiplicative inverse of a mod m
+ */
+Crt.prototype._mulInverse = function(a, m) {
+  egcd = this._egcd(a, m);
+  var d = egcd[0],
+      x = egcd[1],
+      y = egcd[2];
+  if (d !== 1) {
+    throw new Error('Cannot calculate multiplicative inverse of two numbers' +
+        ' that are not coprime: (' + a + ', ' + m + ')');
+  }
+  if (x < 0) {
+    x += m;
+  }
+  return x;
+}
+
+
+/**
+ * Calculates the egcd of two numbers a & b.
+ * @eturns {(Number, Number, Number)} - (gcd, x, y) such that gcd = a*x + b*y
+ */
+Crt.prototype._egcd = function(a, b) {
+  if (b === 0) {
+    return [a, 1, 1];
+  }
+  egcd = this._egcd(b, a % b);
+  var d = egcd[0],
+      x = egcd[1],
+      y = egcd[2];
+  return [d, y, x - (Math.floor(a/b)) * y];
+}
+
+
+Crt.prototype._createSolutionStrTerm = function(prime, mulInverse) {
+    str = this.equations[prime] + '\\cdot (';
+    for (var j = 0; j < this.primes.length; j++) {
+      if (this.primes[j] === prime) {
+        continue;
+      }
+      str += this.primes[j]
+      if (!(j === this.primes.length - 1 ||
+          j === this.primes.length - 2 && this.primes[j+1] === prime)) {
+        str += '\\cdot'
+      }
+    }
+    str += ') \\cdot ' + mulInverse;
+    return str;
+}
+
+
+/**
+ * Solves problem
+ * @returns {[string, Number]) - the string expression of the solution and the
+ *  evaluated answer
+ */
+Crt.prototype.solveProblem = function() {
+  var solutionStr = '';
+  var answer = 0;
+  var prime, productOfOtherPrimes, mulInverse;
+  for (var i = 0; i < this.primes.length; i++) {
+    prime = this.primes[i];
+    productOfOtherPrimes = this.primesProd / prime;
+    mulInverse = this._mulInverse(productOfOtherPrimes, prime);
+    solutionStr += this._createSolutionStrTerm(prime, mulInverse);
+    if (i !== this.primes.length - 1) {
+      solutionStr += '+';
+    }
+    answer += this.equations[prime] * productOfOtherPrimes * mulInverse;
+  }
+  answer = answer % this.primesProd;
+  solutionStr += '\\equiv ' + answer + ' \\pmod{' + this.primesProd + '}';
+  return [solutionStr, answer]
+}
+
+
 angular.module('crtApp', [])
   .controller('ProblemController', ['$scope', function($scope) {
     var prob = new Crt(3);
     $scope.equations = prob.equations;
-    var product = 1;
-    for (var i = 0; i < prob.primes.length; i++) {
-      product *= prob.primes[i];
-    }
-    $scope.primesProd = product;
+    $scope.primesProd = prob.primesProd;
+    var solvedProblem = prob.solveProblem();
+    $scope.solution = solvedProblem[0];
+    $scope.answer = solvedProblem[1];
   }]);
